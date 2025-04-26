45a9fd92f0f8874ddfe3845b717bb841aa2ed9a4
nareshnandan <nareshnandan23@gmail.com>
2025-04-26 21:23:41 +0530

    Modified

--- dice_simulator2.py
+++ dice_simulator2.py
@@ -7,9 +7,9 @@ def roll_dice():
 # Run the dice roll
 roll_dice()
 
-# ----------------------------------
+# ------------------------
 # Simple Dice Model (Reference Only)
 # Dice has 6 faces: 1, 2, 3, 4, 5, 6
 # Used in games like Snakes and Ladders, Ludo, Monopoly, etc.
 # Each time you roll, it gives a random number between 1 and 6
-# ----------------------------------
+# ------------------------
